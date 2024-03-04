8ca50edb2b3089e3a4d2c4ec36ab985fdbbb5e73
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-04 13:37:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-}
+	
+	
+    const lowerCaseStr = s1.toLowerCase();
+    const lowerCaseSubStr = s2.toLowerCase();
 
+    const index = lowerCaseStr.indexOf(lowerCaseSubStr);
+
+   
+    if (index === -1) {
+       
+        for (let i = 0; i < lowerCaseStr.length; i++) {
+            if (lowerCaseSubStr.localeCompare(lowerCaseStr.s2(i, lowerCaseSubStr.length)) < 0) {
+                return i;
+            }
+        }
+       
+        return lowerCaseStr.length;
+    }
+
+    return index;
+	
+}
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:"); 
+
 alert(indexOfIgnoreCase(s1, s2));
+
